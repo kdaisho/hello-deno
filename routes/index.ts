6dbe6dd7de41891bf31bace3b4c82b5f307b21aa
Daisho Komiyama <daishokomiyama@gmail.com>
2022-09-27 18:32:05 -0400

    Update index message

--- routes/index.ts
+++ routes/index.ts
@@ -2,6 +2,6 @@ import { Router } from "../deps.ts";
 
 export function use(path: string, router: Router) {
   router.get(path, ({ response }) => {
-    response.body = { message: "Hello from Deno" };
+    response.body = { message: "Home 🦕" };
   });
 }
