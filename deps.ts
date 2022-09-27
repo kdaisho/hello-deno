6aa4e43f96f32dcc8979576edc402a56689aa402
Daisho Komiyama <daishokomiyama@gmail.com>
2022-09-27 12:52:13 -0400

    Update .gitignroe

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { readJSON, writeJSON } from "https://deno.land/x/flat@0.0.15/mod.ts";
 
-export { Application, Router };
+export { Application, readJSON, Router, writeJSON };
