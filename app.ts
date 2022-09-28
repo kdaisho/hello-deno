7fbe239cc4466c987ec47efb5ea83acfb4821983
Daisho Komiyama <daishokomiyama@gmail.com>
2022-09-28 11:15:53 -0400

    Add cors

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import { Application, Router } from "./deps.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import * as indexRouter from "./routes/index.ts";
 import * as productsRouter from "./routes/products.ts";
 
@@ -6,6 +7,10 @@ const app = new Application();
 const router = new Router();
 const port = 3000;
 
+app.use(oakCors({
+  origin: /^.+localhost:(5173|5174)/,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
 app.use(router.routes());
 app.use(router.allowedMethods());
 
