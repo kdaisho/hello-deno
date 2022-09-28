a4103d8ef5155ae6bd45aa722fc92f7d4650c620
Daisho Komiyama <daishokomiyama@gmail.com>
2022-09-28 14:48:10 -0400

    Replace undefined pkgs2205 with pkgsUnstable

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 let
   pkgsUnstable = import (fetchTarball "http://nixos.org/channels/nixos-unstable/nixexprs.tar.xz") {};
 in
-  pkgs2205.mkShell {
+  pkgsUnstable.mkShell {
     buildInputs = [
       pkgsUnstable.deno
       pkgsUnstable.docker-compose
