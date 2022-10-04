83271fef08d86f4049472cf2b5dfa03a69771ed2
kdaisho <daishokomiyama@gmail.com>
2022-10-04 14:26:39 -0400

    Add logs

--- routes/index.ts
+++ routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "../deps.ts";
 
 export function use(path: string, router: Router) {
+  console.log('======================== 0', { path, router })
   router.get(path, ({ response }) => {
+    console.log('======================== 1', { response })
     response.body = { message: "Home 🦕" };
   });
 }
