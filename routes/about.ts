805f99c776ce844111d8c9e28f54b4041b4230a9
kdaisho <daishokomiyama@gmail.com>
2022-10-11 15:40:15 -0400

    Add add api support

--- routes/about.ts
+++ routes/about.ts
@@ -1,6 +1,11 @@
 import { Router } from "../deps.ts";
 
 export function use(path: string, router: Router) {
+  router.post(`${path}/add`, async ({ request, response }) => {
+    const { a, b } = await request.body().value;
+    response.body = a + b;
+  });
+
   router.get(`${path}/recipe`, ({ response }) => {
     response.body = {
       title: "Curry",
